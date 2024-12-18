8c4edac067c3539edf4f095fe74d8dec1fb1ebc5
Simple Chance <cwjewell19@gmail.com>
2024-12-18 12:12:46 -0500

    added nav bar and hamburger menu, fixed section fade animation

--- js/index.js
+++ js/index.js
@@ -1,21 +1,28 @@
-// Simple script to fade in sections on the landing page
+// Simple script to fade in sections on the landing page and toggle hamburger menu for small screens
+
+// Navbar toggle for mobile
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
 
 // Select all sections
 const sections = document.querySelectorAll('section');
 
-// Create an Intersection Observer
+// Create an Intersection Observer for section visibility of sections
 const observer = new IntersectionObserver(
-  (entries, observer) => {
+  (entries) => {
     entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible'); // Add the 'visible' class
-      }
-      else {
-        entry.target.classList.remove('visible'); //Remove the 'visible' class
+      if (entry.intersectionRatio >= 0.25) {
+        entry.target.classList.add('visible');
+      } else if (entry.intersectionRatio <= 0.15){
+        entry.target.classList.remove('visible');
       }
     });
   },
-  { threshold: 0.25 } // Trigger when 20% of the section is visible
+  { threshold: [0.15, 0.25] }
 );
 
 // Observe each section
