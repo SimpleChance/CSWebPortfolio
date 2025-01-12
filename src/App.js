f3bfbc3bae48669b63e653b94274e62fcad6eb5e
Simple Chance <cwjewell19@gmail.com>
2025-01-12 07:23:47 -0500

    Altered index.js and app.js to handle HashRouting properly for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import LandingPage from './pages/LandingPage';
@@ -7,17 +7,15 @@ import Experiments from './pages/Experiments';
 
 const App = () => {
   return (
-    <Router>
-      <div className="app">
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<LandingPage />} />
-          <Route path="/CSWebPortfolio" element={<LandingPage />} />
-          <Route path="/experiments" element={<Experiments />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/CSWebPortfolio" element={<LandingPage />} />
+        <Route path="/experiments" element={<Experiments />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
+import { HashRouter } from 'react-router-dom';
 import './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
