15e7708b482b602f33fd4bbb1c029aa1cf6a0ea7
Simple Chance <cwjewell19@gmail.com>
2025-01-12 07:15:38 -0500

    Changed BrowserRouter to HashRouter to work with gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouterRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import LandingPage from './pages/LandingPage';
