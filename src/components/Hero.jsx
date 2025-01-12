9cb143e74cd6b2e969d13790788629dcd30e4f03
Simple Chance <cwjewell19@gmail.com>
2025-01-12 07:40:04 -0500

    Small formatting fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <header className="hero">
       <h1>Hi, I'm Chance!</h1>
       <p>Welcome to my personal website. Explore my journey, projects, and aspirations in technology and beyond.</p>
-      <li><Link to="/experiments" className="cta-button"> Explore Demos </Link></li>
+      <Link to="/experiments" className="cta-button"> Explore Demos </Link>
     </header>
   );
 }
